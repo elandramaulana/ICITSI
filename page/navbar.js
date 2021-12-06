8ef592cb296688668f05a026caa14746f934b8d4
Elandra <elandramaulana81@gmail.com>
2021-12-07 02:31:01 +0700

    new

--- page/navbar.js
+++ page/navbar.js
@@ -9,9 +9,9 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    if (window.pageYOffset >= 40) {
-        navbar.classList.replace("nav-awal", "nav")
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
     } else {
-        navbar.classList.replace("nav", "nav-awal");
+        navbar.classList.remove("sticky");
     }
 }
